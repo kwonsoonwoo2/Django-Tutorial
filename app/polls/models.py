a25fd313ae738333db90f8ab24a9a01db5e2e2dd
ksw <zenuine1@gmail.com>
2018-10-02 15:46:56 +0900

    Question에 was_published_recently()함수 구현, Question, Choice의 __str__() 설정

--- app/polls/models.py
+++ app/polls/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('게시일자')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        """
+        최근에 게시되었는지 여부를 리턴
+
+        자신의 게시일자 >= 지금 - 1일
+        :return:
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+    )
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
